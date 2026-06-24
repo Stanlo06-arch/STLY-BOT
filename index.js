70da564785b165a5429e5a7eb4103b127aebafbc
Stanlo06-arch <Stanlo06@web.de>
2026-06-24 21:43:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent,
+  GatewayIntentBits.GuildVoiceStates
+]
 });
 
 require('./commands/willkommen')(client);
